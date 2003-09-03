94a4760d0fef9ee3e1fd8a3c4726167a2daded21
Leigh Smith <leigh@leighsmith.com>
2003-09-02 19:42:15 -0500

    Added versions of these headers which include winheaders.h, otherwise they are identical to those in the Palettes directory. These will be removed when the three state button is purged anyway.

---
+++ Calliope/Application/CalliopeThreeStateButton.h
@@ -0,0 +1,25 @@
+#import "winheaders.h"
+#import <AppKit/AppKit.h>
+
+@interface CalliopeThreeStateButton:NSButton
+{
+}
+
+
++ (void)initialize;
++ (void) setCellClass:(Class)classId;
+- initWithFrame:(NSRect)frameRect;
+
+- (void)setTitle:(NSString *)aString;
+- (void)setImage:(NSImage *)image;
+- (void)setButtonType:(NSButtonType)aType;
+- (int)state;
+- (void)setState:(int)value;
+- (void)performAltClick:sender;
+- (void)performClick:(id)sender;
+- (void)setCyclic:(int)value;
+- (int)cyclic;
+-(int) threeState;
+-(void) setThreeState:(int) newState;
+
+@end
