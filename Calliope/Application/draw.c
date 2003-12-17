7b5df7bfa3e68d7077db810759c728798ebbb926
Kim Shrier <kimshrier@users.sourceforge.net>
2003-12-17 04:50:24 +0000

    This file contains stub subroutines for all the postscript in draw.psw.  This allows Calliope to link but does not actually draw anything.

---
+++ Calliope/Application/draw.c
@@ -0,0 +1,66 @@
+#include <string.h>
+
+/* PSWrapsInit() sets up various definitions used by the main wraps */
+void PSWrapsInit( void )
+{
+  return;
+}
+
+
+/* These are what are called from main program */
+void PSmovegray(float x, float y, float g)
+{
+  return;
+}
+
+void PSCmakelineDisplay(float x1, float y1, float x2, float y2)
+{
+  return;
+}
+
+void PSCmakelinePrint(float x1, float y1, float x2, float y2)
+{
+  return;
+}
+
+void PSCstrokeline(float gray, float width)
+{
+  return;
+}
+
+void PSslant(float w, float h, float dy, float x, float y)
+{
+  return;
+}
+
+void PStie(float cx, float cy, float dy, float rh, float hw, float mh, float ln, float a)
+{
+  return;
+}
+
+void PStiedash(float cx, float cy, float dy, float hw, float mh, float ln, float a)
+{
+  return;
+}
+
+void PStietext(float w, float rh, float hw, float mh, float g, float dr)
+{
+  return;
+}
+
+void PSellipse(float cx, float cy, float rx, float ry, float a1, float a2)
+{
+  return;
+}
+
+void PSsetorigin(float cx, float cy, float a)
+{
+  return;
+}
+
+void PSresetorigin( void )
+{
+  return;
+}
+
+
