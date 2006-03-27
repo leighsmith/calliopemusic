e776c0a3d2987ecbe9eed08294778cb4eb777be6
Leigh Smith <leigh@leighsmith.com>
2006-03-27 07:21:37 -0500

    Added log messages when PS functions called

--- Calliope/Application/draw.c
+++ Calliope/Application/draw.c
@@ -1,66 +1,67 @@
+#import <Foundation/Foundation.h>
 #include <string.h>
 
 /* PSWrapsInit() sets up various definitions used by the main wraps */
 void PSWrapsInit( void )
 {
-  return;
+    NSLog(@"Called PSWrapsInit(), needs implementation\n");
 }
 
 
 /* These are what are called from main program */
 void PSmovegray(float x, float y, float g)
 {
-  return;
+    NSLog(@"Called PSmovegray(), needs implementation\n");
 }
 
 void PSCmakelineDisplay(float x1, float y1, float x2, float y2)
 {
-  return;
+    NSLog(@"Called PSCmakelineDisplay(), needs implementation\n");
 }
 
 void PSCmakelinePrint(float x1, float y1, float x2, float y2)
 {
-  return;
+    NSLog(@"Called PSCmakelinePrint(), needs implementation\n");
 }
 
 void PSCstrokeline(float gray, float width)
 {
-  return;
+    NSLog(@"Called PSCstrokeline(), needs implementation\n");
 }
 
 void PSslant(float w, float h, float dy, float x, float y)
 {
-  return;
+    NSLog(@"Called PSslant(), needs implementation\n");
 }
 
 void PStie(float cx, float cy, float dy, float rh, float hw, float mh, float ln, float a)
 {
-  return;
+    NSLog(@"Called PStie(), needs implementation\n");
 }
 
 void PStiedash(float cx, float cy, float dy, float hw, float mh, float ln, float a)
 {
-  return;
+    NSLog(@"Called PStiedash(), needs implementation\n");
 }
 
 void PStietext(float w, float rh, float hw, float mh, float g, float dr)
 {
-  return;
+    NSLog(@"Called PStietext(), needs implementation\n");
 }
 
 void PSellipse(float cx, float cy, float rx, float ry, float a1, float a2)
 {
-  return;
+    NSLog(@"Called PSellipse(), needs implementation\n");
 }
 
 void PSsetorigin(float cx, float cy, float a)
 {
-  return;
+    NSLog(@"Called PSsetorigin(), needs implementation\n");
 }
 
 void PSresetorigin( void )
 {
-  return;
+    NSLog(@"Called PSresetorigin(), needs implementation\n");
 }
 
 
